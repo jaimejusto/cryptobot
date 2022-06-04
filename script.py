3dd5e093ce9910dc26d6f04b89572b5c6e890f30
jaimejusto <jaimejusto@gmail.com>
2022-06-03 18:05:18 -0700

    connected to kucoin futures

--- script.py
+++ script.py
@@ -1,8 +1,20 @@
-import ccxt
-from  dotenv import dotenv
+import os
+import ccxt.async_support as ccxt
+from  dotenv import load_dotenv
 
+# load environment variables
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+API_SECRET = os.getenv('API_SECRET')
+API_PASSWORD = os.getenv('API_PW')
 
-# connect to broker
+# connect to kucoin futures broker
+exchange = ccxt.kucoinfutures({
+    'apiKey': API_KEY,
+    'apiSecret': API_SECRET,
+    'password': API_PASSWORD
+})
+
 
 # get price data
 
